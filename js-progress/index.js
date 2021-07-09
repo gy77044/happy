714f08ee48a043c305c4cddaa2bf1efdeb54caa6
gautam yadav <gy77044@gmail.com>
2021-07-09 21:05:19 +0530

    all done
    .

--- js-progress/index.js
+++ js-progress/index.js
@@ -1,9 +1,30 @@
+for(let i =1;i<=8;i++)
+{
+    if(i%2==0)
+    {
+        for(let j = 1; j<=8;j++)
+        {
+            if(j%2==0)
+            {
+                console.log("#");
+            }
+            else{
+                console.log(" ");
+            }
+        }
 
-for(let i = 1; i<=8;i++)
-   {
-      for(let j=1;j<=8;j++)
-      {
-          console.log('#');
-      }     
-      console.log("\n");         
-   }
+    }
+    else{
+        for(let j = 1; j<=8;j++)
+        {
+            if(j%2==0)
+            {
+                console.log(" ");
+            }
+            else{
+                console.log("#");
+            }
+        }
+
+    }
+}
