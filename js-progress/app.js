dd51f2502486e41df8ba7826565c87618662af76
gautam yadav <gy77044@gmail.com>
2021-07-20 00:28:40 +0530

    mystatis
    myStatis

---
+++ js-progress/app.js
@@ -0,0 +1,16 @@
+const element = document.querySelector("#btn-translate1");
+element.addEventListener("click", btnCounter);
+
+
+function btnCounter()
+{
+    let counter = 1;
+    console.log(counter);
+    
+}
+
+
+btnCounter();
+
+
+
